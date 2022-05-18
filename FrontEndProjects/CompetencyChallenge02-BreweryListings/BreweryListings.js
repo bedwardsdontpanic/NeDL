94b16d7e60f4564516c80c1e5cd3b8e4a7942d79
bedwardsdontpanic <bedwards@dontpaniclabs.com>
2022-05-18 16:09:52 -0500

    wednesday afternoon

--- FrontEndProjects/CompetencyChallenge02-BreweryListings/BreweryListings.js
+++ FrontEndProjects/CompetencyChallenge02-BreweryListings/BreweryListings.js
@@ -1,32 +1,35 @@
 let theCoordinates = "";
 
-
-
-
 async function getBreweries()
 {
-    console.log(theCoordinates);
     let theURL = "https://api.openbrewerydb.org/breweries?by_dist=" + theCoordinates + "&per_page=10";
-    console.log(theURL);
     let response = await fetch(theURL);
     let data = await response.json()
-    console.log(data)
     var toDoTable = document.getElementById('breweriesTable');
     toDoTable.innerHTML = "";
     for(breweries in data)
     {
-
-        let nextValue = data[breweries].website_url + ", " + data[breweries].name + 
-        " address: " + data[breweries].street
         var row = toDoTable.insertRow(-1);
         row.insertCell().appendChild(document.createTextNode(data[breweries].name));
         row.insertCell().appendChild(document.createTextNode(data[breweries].street));
-        var a = document.createElement('a');
-        var linkText = document.createTextNode("website link");
-        a.appendChild(linkText);
-        a.title = "my title text";
-        a.href = data[breweries].website_url;
-        row.insertCell().appendChild(a);        
+        if(data[breweries].website_url != null)
+        {
+            var a = document.createElement('a');
+            var linkText = document.createTextNode("website");
+            a.appendChild(linkText);
+            a.href = data[breweries].website_url;
+            row.insertCell().appendChild(a);    
+        }
+        else 
+        {
+            row.insertCell().appendChild(document.createTextNode("No website."))
+        }
+
+        var a2 = document.createElement('a');
+        var linkText = document.createTextNode("directions");
+        a2.appendChild(linkText);
+        a2.href = "https://www.google.com/maps/dir/" + theCoordinates + "/" + data[breweries].street + " " + data[breweries].city;
+        row.insertCell().appendChild(a2);
     }
 }
 
@@ -34,7 +37,6 @@ async function suggestADrink(){
     let output = document.getElementById('cocktails');
     output.innerHTML = '';
     let theURL = "https://www.thecocktaildb.com/api/json/v1/1/random.php";
-    console.log(theURL);
     let response = await fetch(theURL);
     let data = await response.json()
     output.innerHTML = "<h4><strong>Suggested drink </strong></h4>"
@@ -57,7 +59,6 @@ async function suggestADrink(){
             output.innerHTML += ", " +  data.drinks[cocktails].strIngredient5
         }
         
-
         output.innerHTML += "<br><br><strong>Instructions: </strong>" + data.drinks[cocktails].strInstructions
     }
 }
@@ -69,6 +70,8 @@ function getLocation(callback) {
             theCoordinates = position.coords.latitude + "," + position.coords.longitude;
             user_position.lat = position.coords.latitude; 
             user_position.lng = position.coords.longitude; 
+            latitude = position.coords.latitude;
+            longitude = position.coords.longitude;
             callback(theCoordinates);
         });
     } else {
@@ -76,6 +79,6 @@ function getLocation(callback) {
         }
 }
     getLocation(function(lat_lng){
-        console.log(lat_lng)
         return lat_lng;
     });
+
